61bb25356ebb2138870e98643416f9c2bf40c1e9
Manish6934 <138653268+Manish6934@users.noreply.github.com>
2023-08-09 15:45:06 +0530

    Update manisherror.sol

--- manisherror.sol
+++ manisherror.sol
@@ -1,1 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract EvenOddContract {
+    function requireIsEven(uint256 number) external pure returns (bool) {
+        // Use require to check if the input is valid
+        require(number > 0, "Number must be greater than 0");
+
+        // Check if the number is even
+        require(number % 2 == 0, "Number is not even");
+        return true;
+    }
+
+    function assertIsOdd(uint256 number) external pure returns (bool) {
+        // Use assert to check for internal errors
+        assert(number > 0);
+
+        // Check if the number is odd
+        assert(number % 2 != 0);
+        return true;
+    }
+
+    function revertCheckEvenOrOdd(uint256 number) external pure returns (string memory) {
+        // Use revert to provide custom error messages
+        if (number % 2 == 0) {
+            revert("Number is even");
+        } else {
+            revert("Number is odd");
+        }
+    }
+}
+
 
